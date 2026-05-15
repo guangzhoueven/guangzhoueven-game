c0ace7c365d2de709056810fbaf4bf1bcf2d6771
guangzhoueven <10062132@qq.com>
2026-05-15 22:40:06 +0800

    重大更新，增加了许多游戏，优化了界面

---
+++ gomoku/js/interface.js
@@ -0,0 +1,146 @@
+$(document).ready(function(){
+    var game = new Game($(".go-board"), $(".board tbody"));
+
+    var adjustSize = adjustSizeGen();
+
+    $(window).resize(adjustSize);
+
+    adjustSize();
+    $.mobile.defaultDialogTransition = 'flip';
+    $.mobile.defaultPageTransition = 'flip';
+    
+    $('#mode-select input[type="radio"]').on('change', function(){
+        gameData.mode=$(this).val();
+    });
+    
+    $('#color-select input[type="radio"]').on('change', function(){
+        gameData.color=$(this).val();
+    });
+    
+    $('#level-select input[type="radio"]').on('change', function(){
+        gameData.level=$(this).val();
+    });
+    
+    $('.back-to-game').on('tap',function(){
+        $.mobile.changePage('#game-page');
+    });
+    
+    $("#start-game").on('click',function(){
+        try{
+            game.white.worker.terminate();
+            game.black.worker.terminate();
+        }catch(e){}
+        if(gameData.mode==='vshuman'){
+            game.mode='hvh';
+            game.init(new HumanPlayer("black"), new HumanPlayer("white"));
+        }else{
+            var color, other;
+            if(gameData.color==='black'){
+                color='black';
+                other='white';
+            }else{
+                color='white';
+                other='black';
+            }
+            game.mode=gameData.level;
+            game.init(new HumanPlayer(color), new AIPlayer(game.mode, other));
+        }
+        $.mobile.changePage('#game-page');
+        game.start();
+        setTimeout(function(){$('.back-to-game').button('enable');},100);
+    });
+
+    $("#undo-button").on('tap', function(){
+        game.undo();
+    });
+    
+    $('.fullscreen-wrapper').on('tap', function(){
+        $(this).hide();
+        $.mobile.changePage('#game-won');
+    });
+    
+    $('#new-game').page();
+    $('#game-won').page();
+    gameData.load();
+    $('.back-to-game').button('disable');
+    $.mobile.changePage('#new-game',{changeHash: false});
+
+    window.gameInfo = (function(){
+        var blinking = false,
+            text = "",
+            color = "";
+
+        var self = {};
+
+        self.getBlinking = function(){
+            return blinking;
+        };
+
+        var mainObj = $("#game-info");
+        self.setBlinking = function(val){
+            if(val !== blinking){
+                blinking = val;
+                if(val){
+                    mainObj.addClass("blinking");
+                }else{
+                    mainObj.removeClass("blinking");
+                }
+            }
+        };
+
+        self.getText = function(){
+            return text;
+        };
+
+        var textObj = $("#game-info>.cont");
+        self.setText = function(val){
+            text = val;
+            textObj.html(val);
+        };
+
+        self.getColor = function(){
+            return color;
+        };
+
+        var colorObj = $("#game-info>.go");
+        self.setColor = function(color){
+            colorObj.removeClass("white").removeClass("black");
+            if(color){
+                colorObj.addClass(color);
+            }
+        };
+
+        return self;
+    })();
+});
+
+function showWinDialog(game){
+    gameInfo.setBlinking(false);
+    if(game.mode === 'hvh'){
+        // TODO:汉化
+        var who = (function(string) { 
+            if (string.indexOf('black') != -1) {
+                return '黑子';
+            } else {
+                return '白子';
+            }
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        })(game.getCurrentPlayer().color);
+        $("#game-won h4").html(who+' 赢了!');
+        gameInfo.value = who +' 赢.';
+        $("#win-content").html(who+' 赢了，再来一把？');
+        $('#happy-outer').fadeIn(500);
+    }else{
+        if(game.getCurrentPlayer() instanceof HumanPlayer){
+            $("#game-won h4").html('你赢了！');
+            $("#win-content").html('太厉害了！再来一把？');
+            gameInfo.value='你赢了';
+            $('#sad-outer').fadeIn(500);
+        }else{
+            $("#game-won h4").html('你输了……');
+            $("#win-content").html('真是个战五渣，电脑都打不过……');
+            gameInfo.value='电脑赢了';
+            $('#happy-outer').fadeIn(500);
+        }
+    }
+}
